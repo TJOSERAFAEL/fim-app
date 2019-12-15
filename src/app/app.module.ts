6030a99fe715dcb211594b00b1a8130e77ddb83d
Rafa <ticjoserafael@gmail.com>
2019-12-15 17:11:17 +0100

    Added HttpClientModule and files service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FilesComponent } from './pages/files/files.component';
@@ -16,7 +17,8 @@ import { FilesComponent } from './pages/files/files.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
