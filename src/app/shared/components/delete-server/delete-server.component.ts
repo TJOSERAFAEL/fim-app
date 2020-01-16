f56eadec780bd4cf9408348ebaa707f3ba828839
TJOSERAFAEL <ticjoserafael@gmail.com>
2020-01-16 14:51:54 +0100

    Added server deletion snack bar

--- src/app/shared/components/delete-server/delete-server.component.ts
+++ src/app/shared/components/delete-server/delete-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ServersService } from 'src/app/core/services/servers.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-delete-server',
@@ -11,7 +12,8 @@ export class DeleteServerComponent implements OnInit {
 
   constructor(public dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: string,
-    public serversService: ServersService) { }
+    public serversService: ServersService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit() {
 
@@ -23,7 +25,16 @@ export class DeleteServerComponent implements OnInit {
 
   deleteServer(id: string) {
     console.log(id);
-    this.serversService.deleteServer(id);
+    this.serversService.deleteServer(id).subscribe(data => {
+      if (data) {
+        this.openSnackBar("Server deleted!");
+        this.closeDialog();
+      }
+    });
+  }
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, "CLOSE");
   }
 
 }
